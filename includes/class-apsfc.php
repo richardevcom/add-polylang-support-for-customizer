2684e49cee4b7f0b4841a775d02b5c33bc706c2d
richardevcom <17192512+richardevcom@users.noreply.github.com>
2021-02-02 17:21:36 +0200

    Update class-apsfc.php

--- includes/class-apsfc.php
+++ includes/class-apsfc.php
@@ -275,13 +275,21 @@ class Apsfc {
 	 * @link https://gist.github.com/soderlind/1908634f5eb0c1f69428666dd2a291d0
 	 */
 	public function add_lang_to_customizer_previewer() {
+		$language = ( empty( $_REQUEST['lang'] ) ) ? pll_current_language() : $_REQUEST['lang'];
+
+		if(is_customize_preview() && !isset($_REQUEST['lang'])) {
+			$url = get_admin_url() . 'customize.php?lang=' . $language; // EDIT this to your needs
+			wp_safe_redirect( $url );
+			var_dump($url);
+            exit();
+		}
+		
 		$handle    = 'dss-add-lang-to-template';
 		$src       = plugin_dir_url( dirname(__FILE__) ) . 'assets/js/apsfc.js';
 		$deps      = [ 'customize-controls' ];
 		$version   = rand();
 		$in_footer = 1;
 		wp_enqueue_script( $handle, $src, $deps, $version, $in_footer );
-		$language = ( empty( $_REQUEST['lang'] ) ) ? pll_current_language() : $_REQUEST['lang'];
 
 		if ( empty( $language ) ) {
 			$language = pll_default_language();
